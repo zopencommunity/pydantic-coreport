d53fa882a2f5c369c982a043d45e6b55fe513072
Igor Todorovski <itodorov@ca.ibm.com>
2026-08-10 17:04:16 -0400

    Rename: drop python- prefix from repo name and internal references

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -2,11 +2,11 @@ node('linux') {
   stage ('Poll') {
     checkout([
       $class: 'GitSCM', branches: [[name: '*/main']], extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/python-pydantic-coreport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/pydantic-coreport.git']]])
   }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [
-      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/python-pydantic-coreport.git'),
+      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/pydantic-coreport.git'),
       string(name: 'PORT_DESCRIPTION', value: 'Core functionality for Pydantic validation and serialization (Rust extension, cross-compiled for z/OS)'),
       string(name: 'BUILD_LINE', value: 'DEV'),
       booleanParam(name: 'PUBLISH_PYTHON_WHEEL', value: true)
